71fd23982fbc442648d23c0558e6b79c1228c8f0
Ahmad Ghorbani <56560408+Ahmad-Ghorbani@users.noreply.github.com>
2022-11-08 08:46:04 +0330

    trading view chart: remove console.log

--- src/components/tradingViewChart.js
+++ src/components/tradingViewChart.js
@@ -6,7 +6,7 @@ const TradingViewChart = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.type = "text/jsx";
-    script.src = "../charting_library";
+    script.src = "%PUBLIC_URL%/charting_library/charting_library.js";
     document.head.appendChild(script);
 
     window.tvWidget = new TradingView.widget({
@@ -15,23 +15,10 @@ const TradingViewChart = () => {
       fullscreen: true, // displays the chart in the fullscreen mode
       container: "tv_chart_container",
       datafeed: Datafeed,
-      library_path: "../charting_library/",
+      library_path: "/charting_library/",
     });
-
-    console.log(
-      "__TradingView",
-      (window.tvWidget = new TradingView.widget({
-        symbol: "Bitfinex:BTC/USD", // default symbol
-        interval: "1D", // default interval
-        fullscreen: true, // displays the chart in the fullscreen mode
-        container: "tv_chart_container",
-        datafeed: Datafeed,
-        library_path: "/charting_library/",
-      }))
-    );
-
-    //Do not forget to remove the script on unmounting the component!!
-    // return () => script.remove();
+    //Do not forget to remove the script on unmounting the component!
+    return () => script.remove();
   }, []); //eslint-disable-line
 
   return <div id="tv_chart_container"></div>;
